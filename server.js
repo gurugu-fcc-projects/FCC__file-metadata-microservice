3374b59c7b3a6046dbcf8dc5b6cdddaa6a7517e6
GuRuGu <peter.k.nets@gmail.com>
2021-06-18 11:13:10 +0300

    Send back file details

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const cors = require("cors");
+const multer = require("multer");
+
 require("dotenv").config();
+const upload = multer();
 
 const app = express();
 
@@ -9,6 +12,11 @@ app.use("/public", express.static(process.cwd() + "/public"));
 
 app.get("/", function (req, res) {
   res.sendFile(process.cwd() + "/views/index.html");
+});
+
+app.post("/api/fileanalyse", upload.single("upfile"), (req, res) => {
+  const { originalname: name, mimetype: type, size } = req.file;
+  res.status(201).json({ name, type, size });
 });
 
 const port = process.env.PORT || 3000;
